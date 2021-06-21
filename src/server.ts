1795eb4aaa2149305ef6cf38c9dcbe24792034aa
João Bispo <joaobispo2077@gmail.com>
2021-06-20 21:12:42 -0300

    feat: add base test routes with methods get and post

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get('/test', (req, res) => res.send('Ok'));
+
+app.post('/test', (req, res) => res.send('created'));
+
 const port = 3000;
 app.listen(port, () => console.log(`Server is running at port ${port}`));
