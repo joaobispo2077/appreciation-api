b668f931591e23381beafe30e473a00c68a86a04
João Bispo <joaobispo2077@gmail.com>
2021-06-23 09:23:17 -0300

    feat: add middleware ensure admin into route post tags

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
@@ -9,6 +10,6 @@ router.post('/users', createUserController.handle);
 
 
 const createTagController = new CreateTagController();
-router.post('/tags', createTagController.handle);
+router.post('/tags', ensureAdmin, createTagController.handle);
 
 export {router};
