da2ef2770ea80d56ff3ea8d656e33eb9b96749de
João Bispo <joaobispo2077@gmail.com>
2021-06-25 09:27:02 -0300

    feat: add middleware ensure admin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,11 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(req: Request,res: Response, next:NextFunction) {
-  const admin = true;
+export async function ensureAdmin(req: Request,res: Response, next:NextFunction) {
+  const { user_id } = req;
+  console.log('id user: ' + user_id);
 
-  if(admin) {
+  const usersRepositories = getCustomRepository(UsersRepositories);
+  const user = await usersRepositories.findOne({id: user_id});
+  
+  if(user.admin) {
     return next();
   }
+
+  console.log(user.admin);
 
   return res.status(401).json({
     error: "Unauthorized."
