0ade4439714c5570b82722e3f609c729fa0f2fc0
João Bispo <joaobispo2077@gmail.com>
2021-06-25 10:26:50 -0300

    feat: add customize class Tag attributtes

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -1,5 +1,6 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
 import { v4 as uuid } from 'uuid';
+import { Expose } from 'class-transformer';
 
 @Entity('tags')
 class Tag {
@@ -14,6 +15,12 @@ class Tag {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+
+  @Expose({ name: 'nameCustom'})
+  nameCustom(): string {
+    return `#${this.name}`
+  };
 
   constructor() {
     if(!this.id) this.id = uuid();
--- src/services/ListTagsService.ts
+++ src/services/ListTagsService.ts
@@ -1,13 +1,13 @@
 import { getCustomRepository } from "typeorm";
 import { TagsRepositories } from "../repositories/TagsRepositories";
-
+import { classToPlain } from 'class-transformer';
 class ListTagsService {
   async execute() {
     const tagsRepository = getCustomRepository(TagsRepositories);
 
     const tags = await tagsRepository.find();
 
-    return tags;
+    return classToPlain(tags);
   }
 }
 
